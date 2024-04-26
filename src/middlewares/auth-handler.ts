709d29c9bb6a1c0dc9eb141dec746254eb85836e
Chamith Jayaweera <chamithlkml@gmail.com>
2024-04-26 09:20:42 +0100

    Role based access control for products route

--- src/middlewares/auth-handler.ts
+++ src/middlewares/auth-handler.ts
@@ -13,6 +13,7 @@ class AuthHandler
 
     this.authUser = this.authUser.bind(this);
     this.getUser = this.getUser.bind(this);
+    this.authProducts = this.authProducts.bind(this);
   }
 
   private async getUser(authorization: string | undefined){
@@ -44,14 +45,25 @@ class AuthHandler
   public async authUser(req: Request, res: Response, next: NextFunction)
   {
     try{
-      const user = await this.getUser(req.headers.authorization)
+      req.body.user = await this.getUser(req.headers.authorization);
   
-      if(!user){
-        throw new BadRequestsException('Authentication failed.', ErrorCode.AUTHENTICATION_ERROR, 400);
+      next();
+    }catch(error){
+      next(error);
+    }
+  }
+
+  public async authProducts(req: Request, res: Response, next: NextFunction)
+  {
+    try{
+      const user = await this.getUser(req.headers.authorization);
+
+      if(user.role != 'ADMIN'){
+        throw new BadRequestsException('You are not authorised to perform this', ErrorCode.USER_NOT_ALLOWED, 400);
       }
 
       req.body.user = user;
-  
+
       next();
     }catch(error){
       next(error);
