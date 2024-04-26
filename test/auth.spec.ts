8a25737510c14f5b129b3d7aba511b2670bb526a
Chamith Jayaweera <chamithlkml@gmail.com>
2024-04-26 12:12:38 +0100

    Added a test for GET /api/auth/me

--- test/auth.spec.ts
+++ test/auth.spec.ts
@@ -7,7 +7,7 @@ const email = faker.internet.email();
 const password = faker.internet.password();
 
 describe('POST /api/auth/signup', () => {
-  it('Should return status 200', (done) => {
+  it('Should signup user', (done) => {
     const userData: {
       email: string,
       password: string,
@@ -34,7 +34,7 @@ describe('POST /api/auth/signup', () => {
 });
 
 describe('POST /api/auth/signin', () => {
-  it('Should return status 200', (done) => {
+  it('Should sign in user', (done) => {
     const userData: {
       email: string,
       password: string
@@ -50,8 +50,45 @@ describe('POST /api/auth/signin', () => {
               if(err) return done(err);
 
               should(res.body).have.property('id').and.be.a.Number();
-
+              should(res.body).have.property('token').and.be.a.String();
               done();
+            });
+  });
+});
+
+describe('GET /api/auth/me', () => {
+  it('Should authenticate user with auth token', (done) => {
+    const userData: {
+      email: string,
+      password: string
+    } = {
+      email: email,
+      password: password
+     };
+     request(app)
+            .post('/api/auth/signin')
+            .send(userData)
+            .expect(200)
+            .end((err, res) => {
+              if(err) return done(err);
+
+              should(res.body).have.property('id').and.be.a.Number();
+              should(res.body).have.property('token').and.be.a.String();
+
+              const authToken: string = res.body.token;
+
+              request(app)
+                .get('/api/auth/me')
+                .set('Authorization', authToken)
+                .send()
+                .expect(200)
+                .end((err, res) => {
+                  if(err) return done(err);
+
+                  should(res.body).have.property('id').and.be.a.Number();
+
+                  done();
+                })
             });
   });
 });
